5425d2371ef9c3d05d3fc8c8c21b4833a155b254
silvia-YQY <545067444@qq.com>
2018-08-31 23:21:14 +0800

    数据结构

--- Lesson3/src/com/LessonThree/java/FackStack.java
+++ Lesson3/src/com/LessonThree/java/FackStack.java
@@ -6,17 +6,20 @@ public class FackStack {
     private Stack<Integer> stack = new Stack<Integer>();
     private LinkedList<Integer> queue = new LinkedList<Integer> ();
     //栈：先进后出, 后进先出
+    // 深度优先搜索！！ 本质就是栈的使用
     public void runStack(){
         //插入
         stack.push(5);
         stack.push(3);
-        // 取出
+
+        // 只看顶部数据，但是不删除
+        System.out.println("stack.peek ^^^^^^"+stack.peek());
+
+        // 取出，即删除
         System.out.println(stack.pop()); // 3
         System.out.println(stack.pop()); // 5
 
-        // 深度优先搜索！！ 本质就是栈的使用
-        // 只看顶部数据，但是不删除
-        // System.out.println("stack.peek ^^^^^^"+stack.peek());
+
         // 检查是否为空
         System.out.println("stack.isEmpty ^^^^^^"+stack.isEmpty());
 
@@ -27,14 +30,13 @@ public class FackStack {
         //插入
         queue.add(30);
         queue.add(50);
+        // 只看顶部数据，但是不删除
+        System.out.println("queue.peek……"+queue.peek());
         // 取出
         System.out.println(queue.pop()); //  30
         System.out.println(queue.pop()); //  50
 
-        Integer First_OF_queue = (Integer) queue.peek();
-        System.out.println("peek"+queue.peek()); //  50
-
-        System.out.println("peek"+queue.isEmpty()); //  判断是否为空
+        System.out.println("queue.isEmpty……"+queue.isEmpty()); //  判断是否为空
 
     }
 
