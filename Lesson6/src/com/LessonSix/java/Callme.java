b7a9f67ad44a50e149120fa0724b4c145e64d1e3
silvia-YQY <545067444@qq.com>
2018-09-15 22:30:32 +0800

    多线程资源共享，同步

---
+++ Lesson6/src/com/LessonSix/java/Callme.java
@@ -0,0 +1,14 @@
+package com.LessonSix.java;
+
+// This program is not synchronized.
+class Callme {
+    synchronized void call(String msg) {
+        System.out.print("[" + msg);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println("]");
+    }
+}
